437ee85fd4d4fc69f32d21507aa69b9a8dddb26f
vipzero <hiroflighter@gmail.com>
2019-05-02 18:53:27 +0900

    init

---
+++ scripts/hosyu.ts
@@ -0,0 +1,21 @@
+import puppeteer from "puppeteer"
+
+async function main() {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  })
+  const page = await browser.newPage()
+  const threadURL = "http://hebi.5ch.net/test/read.cgi/news4vip/1556625403"
+  await page.goto(threadURL)
+  await page.type("form textarea", "ほ")
+  await page.click("form [type=submit]")
+  await page.waitForSelector("input[value=上記全てを承諾して書き込む]")
+
+  await page.click("input[type=submit]")
+
+  page.close()
+  console.log("posted" + new Date())
+}
+
+setInterval(main, 5 * 60 * 1000)
